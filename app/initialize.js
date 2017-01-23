b4a2a73a43e49a0e6ffd156ebd52f3f4619afe72
Tendai Mutunhire <tmutunhire@gmail.com>
2017-01-23 22:26:13 +0200

    logger

--- app/initialize.js
+++ app/initialize.js
@@ -2,3 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
 });
+
+console.log("Whats goin on here?");
---
+++ app/logger.js
@@ -0,0 +1,2 @@
+console.log('Hello, world');
+console.log('doesnt make any sense');
